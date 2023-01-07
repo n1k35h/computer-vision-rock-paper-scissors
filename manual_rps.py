022d44284ce5d286144c01dd32e12c910719c20e
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-01-07 22:12:21 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -9,3 +9,42 @@ def get_user_choice():
     user_input = input("What is your choice? ")
     print(f"User input is: {user_input}")
     return user_input
+
+def get_winner(computer_choice=str, user_choice=str):
+    if computer_choice == "Rock" and user_choice == "Rock":
+        print("Computer chooses Rock")
+        print("It is a tie!")
+
+    elif computer_choice == "Rock" and user_choice == "Scissors":
+        print("Computer choose Rock")
+        print("You lost")
+
+    elif computer_choice == "Rock" and user_choice == "Paper":
+        print("Computer choose Rock")
+        print("You won!")
+    
+    if computer_choice == "Paper" and user_choice == "Paper":
+        print("Computer chooses Paper")
+        print("It is a tie!")
+
+    elif computer_choice == "Paper" and user_choice == "Scissors":
+        print("Computer choose Paper")
+        print("You won!")
+
+    elif computer_choice == "Paper" and user_choice == "Rock":
+        print("Computer choose Paper")
+        print("You lost")
+
+    if computer_choice == "Scissors" and user_choice == "Scissors":
+        print("Computer chooses Scissors")
+        print("It is a tie!")
+
+    elif computer_choice == "Scissors" and user_choice == "Rock":
+        print("Computer choose Scissors")
+        print("You won!")
+
+    elif computer_choice == "Scissors" and user_choice == "Paper":
+        print("Computer choose Scissors")
+        print("You lost")
+    else:
+        ("Please enter a valid choice!")
