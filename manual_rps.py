8431399b611589a87a33aa4b5fd5b2e0229f045b
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-01-07 18:17:05 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -1,8 +1,11 @@
 import random
 
 def get_computer_choice():
-    pass
+    computer_choice = random.choice(["Rock", "Paper", "Scissors", "Nothing"])
+    print(f"The Computer's choice is: {computer_choice}")
+    return computer_choice
 
 def get_user_choice():
-    pass
-
+    user_input = input("What is your choice? ")
+    print(f"User input is: {user_input}")
+    return user_input
