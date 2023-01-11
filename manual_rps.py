7bc896b4a7df4246a7b96726360d3d1d1b350a27
n1k35h <119499198+n1k35h@users.noreply.github.com>
2023-01-11 22:55:14 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -12,39 +12,47 @@ def get_user_choice():
 
 def get_winner(computer_choice=str, user_choice=str):
     if computer_choice == "Rock" and user_choice == "Rock":
-        print("Computer chooses Rock")
-        print("It is a tie!")
+        # print("Computer chooses Rock")
+        print("\nIt is a tie!")
 
     elif computer_choice == "Rock" and user_choice == "Scissors":
-        print("Computer choose Rock")
-        print("You lost")
+        # print("Computer choose Rock")
+        print("\nYou lost")
 
     elif computer_choice == "Rock" and user_choice == "Paper":
-        print("Computer choose Rock")
-        print("You won!")
+        # print("Computer choose Rock")
+        print("\nYou won!")
     
-    if computer_choice == "Paper" and user_choice == "Paper":
-        print("Computer chooses Paper")
-        print("It is a tie!")
+    elif computer_choice == "Paper" and user_choice == "Paper":
+        # print("Computer chooses Paper")
+        print("\nIt is a tie!")
 
     elif computer_choice == "Paper" and user_choice == "Scissors":
-        print("Computer choose Paper")
-        print("You won!")
+        # print("Computer choose Paper")
+        print("\nYou won!")
 
     elif computer_choice == "Paper" and user_choice == "Rock":
-        print("Computer choose Paper")
-        print("You lost")
+        # print("Computer choose Paper")
+        print("\nYou lost")
 
-    if computer_choice == "Scissors" and user_choice == "Scissors":
-        print("Computer chooses Scissors")
-        print("It is a tie!")
+    elif computer_choice == "Scissors" and user_choice == "Scissors":
+        # print("Computer chooses Scissors")
+        print("\nIt is a tie!")
 
     elif computer_choice == "Scissors" and user_choice == "Rock":
-        print("Computer choose Scissors")
-        print("You won!")
+        # print("Computer choose Scissors")
+        print("\nYou won!")
 
     elif computer_choice == "Scissors" and user_choice == "Paper":
-        print("Computer choose Scissors")
-        print("You lost")
+        # print("Computer choose Scissors")
+        print("\nYou lost")
     else:
-        ("Please enter a valid choice!")
+        ("\nPlease enter a valid choice!")
+
+def play():
+    user_choice = get_user_choice()     # Calls the get_user_choice function
+    computer_choice = get_computer_choice()     # calls the get_computer_choice function
+
+    return get_winner(computer_choice=computer_choice, user_choice=user_choice)     # calls the get_winner function
+
+play()
