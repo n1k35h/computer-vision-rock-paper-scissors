1c6a8f7d6b5578af02eff808de2bd2a3e6c55fee
nikesh mistry <nikeshmistry83@gmail.com>
2023-01-23 20:42:43 +0000

    format change

--- camera_rps.py
+++ camera_rps.py
@@ -80,41 +80,41 @@ def get_computer_choice():
 def get_winner(computer_choice=str, user_choice=str):
     if computer_choice == "Rock" and user_choice == "Rock":
         # print("Computer chooses Rock")
-        print("\nIt is a tie!")
+        print("It is a tie!")
 
     elif computer_choice == "Rock" and user_choice == "Scissors":
         # print("Computer choose Rock")
-        print("\nYou lost")
+        print("You lost")
 
     elif computer_choice == "Rock" and user_choice == "Paper":
         # print("Computer choose Rock")
-        print("\nYou won!")
+        print("You won!")
     
     elif computer_choice == "Paper" and user_choice == "Paper":
         # print("Computer chooses Paper")
-        print("\nIt is a tie!")
+        print("It is a tie!")
 
     elif computer_choice == "Paper" and user_choice == "Scissors":
         # print("Computer choose Paper")
-        print("\nYou won!")
+        print("You won!")
 
     elif computer_choice == "Paper" and user_choice == "Rock":
         # print("Computer choose Paper")
-        print("\nYou lost")
+        print("You lost")
 
     elif computer_choice == "Scissors" and user_choice == "Scissors":
         # print("Computer chooses Scissors")
-        print("\nIt is a tie!")
+        print("It is a tie!")
 
     elif computer_choice == "Scissors" and user_choice == "Rock":
         # print("Computer choose Scissors")
-        print("\nYou won!")
+        print("You won!")
 
     elif computer_choice == "Scissors" and user_choice == "Paper":
         # print("Computer choose Scissors")
-        print("\nYou lost")
+        print("You lost")
     else:
-        ("\nPlease display a valid option to the Camera!")
+        ("Please display a valid option to the Camera!")
 
 def play():
     user_choice = get_prediction()     # Calls the get_prediction function
